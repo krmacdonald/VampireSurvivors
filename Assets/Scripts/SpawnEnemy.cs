d986c93955c07ba2473d49e721f89d1a78ff02f5
burntclowns <143651136+burntclowns@users.noreply.github.com>
2023-10-25 16:15:56 -0400

    ksljdf
    jdfkl

---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public LayerMask buttonLayer; // Detecting the layer. We probably will not need this down the line. 
+    public GameObject enemyPrefab; // This is where enemies go
+    public GameObject spawnObject; //just an empty game object to reference where to spawn this enemy
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // casting a ray
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, buttonLayer))
+            {
+                // Check if the ray hit a button
+                ButtonClicked(hit.collider.gameObject);
+                Debug.Log("button was hit");
+            }
+        }
+    }
+
+    void ButtonClicked(GameObject button)
+    {
+        // Generate the enemy. This can be flexible for when we just want to spawn enemies generally. I just have this raycast running for the purpose of testing
+        Instantiate(enemyPrefab, spawnObject.transform.position, spawnObject.transform.rotation);
+    }
+}

57bf7985ec6f08bd53333f1d938bf4390d5cdbf2
krmacdonald <krmacdonald@quinnipiac.edu>
2023-11-05 03:57:06 -0500

    Added muzzle flash to gun + tweaked prefabs
    still need to program enemy AI

--- Assets/Scripts/Weapon Scripts/gunScript.cs
+++ Assets/Scripts/Weapon Scripts/gunScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Playables;
 using UnityEngine;
 
 public class gunScript : MonoBehaviour
@@ -7,24 +8,56 @@ public class gunScript : MonoBehaviour
     public Transform camTransform;
     public float gunDamage;
     public float gunFirerate;
+    private float gunDelay;
     public int magazine;
     public float reloadSpeed;
     private basicEnemyBehavior enemyScript;
+    public PlayableDirector director;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) //m1 detection
+        
+        if (gunDelay > gunFirerate) //Checks if gun is ready to fire
         {
-            RaycastHit rayHit; //sends out raycast
-            if (Physics.Raycast(camTransform.position, camTransform.forward, out rayHit))
+            if (Input.GetMouseButtonDown(0)) //m1 detection
             {
-                if (rayHit.collider.gameObject.tag == "Enemy")
+                director.Stop();
+                director.Play(); //creates muzzle flash
+                RaycastHit rayHit; //sends out raycast
+                Shake(3f, 5f);
+                if (Physics.Raycast(camTransform.position, camTransform.forward, out rayHit)) 
                 {
-                    enemyScript = rayHit.collider.gameObject.GetComponent<basicEnemyBehavior>(); //grabs the enemy's script
-                    enemyScript.takeDamage(gunDamage); //broadcasts gundamage to enemy's takedamage method
+                    if (rayHit.collider.gameObject.tag == "Enemy")
+                    {
+                        enemyScript = rayHit.collider.gameObject.GetComponent<basicEnemyBehavior>(); //grabs the enemy's script
+                        enemyScript.takeDamage(gunDamage); //broadcasts gundamage to enemy's takedamage method
+                    }
                 }
             }
         }
+        else
+        {
+            gunDelay += Time.deltaTime;
+        }
+    }
+
+    //Handles camera shake
+    public void Shake(float duration, float magnitude)
+    {
+        Vector3 originalPosition = camTransform.position;
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            camTransform.position = new Vector3(originalPosition.x + x, originalPosition.y + y, originalPosition.z);
+
+            elapsed += Time.deltaTime;
+        }
+
+        camTransform.position = originalPosition;
     }
 }

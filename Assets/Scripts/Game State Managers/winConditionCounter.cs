795523e3835bffe68ca7b947cdf3e99e8c798941
krmacdonald <krmacdonald@quinnipiac.edu>
2023-11-14 16:46:44 -0500

    Added text for objective progress

--- Assets/Scripts/Game State Managers/winConditionCounter.cs
+++ Assets/Scripts/Game State Managers/winConditionCounter.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class winConditionCounter : MonoBehaviour
 {
     public int numEnemies;
     public int curEnemies;
+    public GameObject text;
+    private TextMeshProUGUI actualText;
     void Start()
     {
-        
+        actualText = text.GetComponent<TextMeshProUGUI>();
+        curEnemies = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        actualText.text = "Cultists Purged\n" + curEnemies + " / " + numEnemies;
         if(numEnemies == curEnemies)
         {
             endGame();

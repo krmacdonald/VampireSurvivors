00ae5ceab1fc3470ad4996d8a918d8304e9a1478
krmacdonald <krmacdonald@quinnipiac.edu>
2023-11-14 11:35:43 -0500

    Cross now updates w repentance

--- Assets/Scripts/Player Scripts/HUDScript.cs
+++ Assets/Scripts/Player Scripts/HUDScript.cs
@@ -8,16 +8,20 @@ public class HUDScript : MonoBehaviour
     public playerLifeManager lifeScript;
     public Sprite[] beadSprites;
     public GameObject beadImage;
+    public GameObject crossMask;
+    private RectTransform maskSize;
     private int spriteIndex;
     // Start is called before the first frame update
     void Start()
     {
-        
+        maskSize = crossMask.GetComponent<RectTransform>();
+        maskSize.sizeDelta = new Vector2(100, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        maskSize.sizeDelta = new Vector2(100, lifeScript.repentance);
         spriteIndex = (int)(lifeScript.health / 5.88235294);
         if(spriteIndex == 0 && lifeScript.health != 0)
         {
@@ -25,4 +29,6 @@ public class HUDScript : MonoBehaviour
         }
         beadImage.GetComponent<Image>().sprite = beadSprites[17-spriteIndex];
     }
+
+
 }

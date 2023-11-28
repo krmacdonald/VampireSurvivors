d1e9843ad870a6e8512feea11558b9db2a3bc7b2
krmacdonald <krmacdonald@quinnipiac.edu>
2023-11-28 18:06:43 -0500

    placeholders for weapons in weapon testing, can be bettered later

---
+++ Assets/Scripts/Weapon Scripts/shotgunScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shotgunScript : MonoBehaviour
+{
+    public int bulletDamage;
+    public int projectileAmount;
+    public float fireSpeed;
+    private float fireSpeedDelay;
+    public AudioSource fireSound;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

5bd5d9a0a87e39959cf03e8ad75c45d8e2319499
krmacdonald <krmacdonald@quinnipiac.edu>
2023-11-30 15:07:15 -0500

    launcher script done, need to code grenade

--- Assets/Scripts/Weapon Scripts/grenadeLauncher.cs
+++ Assets/Scripts/Weapon Scripts/grenadeLauncher.cs
@@ -7,6 +7,11 @@ public class grenadeLauncher : MonoBehaviour
     public GameObject grenadePrefab;
     public float firerate;
     private float firerateTimer = 0;
+    private GameObject instantiatedGrenade;
+    public playerLifeManager healthGetter;
+    public float grenadeLaunchSpeed;
+    private Rigidbody grenadeRigidbody;
+    public Transform camTrans;
 
 
     void Start()
@@ -17,6 +22,24 @@ public class grenadeLauncher : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        firerateTimer += Time.deltaTime;
+        if (healthGetter.isAlive)
+        {
+            if (firerateTimer > firerate)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    fireGrenade();
+                    firerateTimer = 0;
+                }
+            }
+        }
+    }
+    
+    void fireGrenade()
+    {
+        instantiatedGrenade = Instantiate(grenadePrefab, this.transform.position + new Vector3(0, .2f, 0) + this.transform.forward + (this.transform.right/8), this.transform.rotation);
+        grenadeRigidbody = instantiatedGrenade.GetComponent<Rigidbody>();
+        grenadeRigidbody.AddForce((camTrans.forward + new Vector3(0, .23f, 0)) * grenadeLaunchSpeed);
     }
 }

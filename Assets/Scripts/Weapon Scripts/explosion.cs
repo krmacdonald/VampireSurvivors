edf0956a244bc76f6f1366ff67f9843e570c4c8d
DarrellJ73 <galantgame51@gmail.com>
2023-12-04 15:54:39 -0500

    audio effects

--- Assets/Scripts/Weapon Scripts/explosion.cs
+++ Assets/Scripts/Weapon Scripts/explosion.cs
@@ -6,14 +6,17 @@ public class explosion : MonoBehaviour
 {
     public float explosionDecay;
     private float explosionCounter;
+    AudioSource m_shootingsound;
+
     void Start()
     {
-        
+        AudioSource m_shootingsound;
     }
 
     // Update is called once per frame
     void Update()
     {
+        AudioSource m_shootingsound;
         explosionCounter += Time.deltaTime;
         if(explosionCounter > explosionDecay)
         {
